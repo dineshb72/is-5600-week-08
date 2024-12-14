b6cdf9802ea5ec972bf82715747a256d99a45e54
dineshb72 <bukyadinesh72@gmail.com>
2024-12-14 05:09:51 +0000

    week8 commits

---
+++ tests/app.test.js
@@ -0,0 +1,39 @@
+//const { describe, it } = require('node:test');
+const request = require('supertest');
+const app = require('../app.js');
+
+//const { hasUncaughtExceptionCaptureCallback } = require('process');
+
+
+describe("The express server" , () => {
+   beforeAll(done => {
+    done();
+   });
+
+
+
+
+   test('should return response' , async () => {
+
+    const res = await request(app)
+    .get('/');
+    expect(res.statusCode).toEqual(200);   
+});
+
+test('should respond at /products' , async () => {
+    const res = await request(app).get('/products');
+
+    expect(res.statusCode).toEqual(200);
+});
+
+
+test('should respond at /orders ', async () => {
+
+    const res = await request(app).get('/orders');
+
+    expect(res.statusCode).toEqual(200);
+} )
+
+
+})
+
